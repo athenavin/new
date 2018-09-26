7783866b2851ec677a033794df83d8b378f33939
athenavin <vinayak.vyas@athenalogics.com>
2018-09-26 11:57:42 +0000

    neee

--- shadow.js
+++ shadow.js
@@ -1,9 +1,9 @@
 var awsIot = require('aws-iot-device-sdk');
 
 var thingShadows = awsIot.thingShadow({
-	keyPath:"397e72249c-private.pem.key",
-	certPath:"397e72249c-certificate.pem.key",
-	caPath:"AmazonRootCA1.pem",
+	keyPath:"/home/pi/deviceSDK/certs/397e72249c-private.pem.key",
+	certPath:"/home/pi/deviceSDK/certs/397e72249c-certificate.pem.crt",
+	caPath:"/home/pi/deviceSDK/certs/AmazonRootCA1.pem",
 	clientId:"first",
 	host:"a2zzcbsmssfhbu.iot.ap-south-1.amazonaws.com"
 });
@@ -14,11 +14,12 @@ thingShadows.on('connect', function() {
 
   console.log('connected');
 
-  thingShadows.register("first",{},function(){console.log("registered");});
+  thingShadows.register("first",{},function(){console.log("registered");
   let value = {"state":{"desired":{"value":"output"}}};
   status = thingShadows.update("first", value);
-  if(value === null){console.log("update not done");}
+  if(status === null){console.log("update not done");}
 
+});
 });
 
 thingShadows.on('status',function(thingName, stat, clientToken, stateObject){
@@ -26,6 +27,8 @@ thingShadows.on('status',function(thingName, stat, clientToken, stateObject){
 
 });
 
+thingShadows.on('delta', function(thingName, stateObject){
+	console.log("delta_is:"+JSON.stringify(stateObject));
 
-
-//thingShadows.unregister("first");
+thingShadows.unregister("first");
+});
